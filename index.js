d764c37358aa0c0724f03652e2d970e715712424
peter-sila1 <peter.sila@student.moringaschool.com>
2023-08-07 14:47:48 +0300

    completed fetch

--- index.js
+++ index.js
@@ -1,17 +1,30 @@
+@ -1,5 +1,13 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  return fetch("https://anapioficeandfire.com/api/books")
+    .then((response) => response.json())
+    .then((data) => {
+      renderBooks(data);
+    })
+    .catch((error) => {
+      console.log("Error fetching books:", error);
+    });
+
+
 }
 
-function renderBooks(books) {
-  const main = document.querySelector('main');
-  books.forEach(book => {
+@@ -9,9 +17,11 @@ function renderBooks(books) {
     const h2 = document.createElement('h2');
     h2.innerHTML = book.name;
     main.appendChild(h2);
   });
+  })
+
 }
 
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
 });
+   
+
+
